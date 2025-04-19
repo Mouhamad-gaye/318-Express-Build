1fb77243a9b60294d394ab5aa939f7629382392a
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-04-19 16:54:47 -0600

    fourth commit

--- 318.2.1 SBA App/routes/books.mjs
+++ 318.2.1 SBA App/routes/books.mjs
@@ -4,20 +4,35 @@ const router = express.Router();
 // Routes
 router
 .route('/books')
-.get((req, res) => {
-    res.send('View books');
+.post('/books', (req, res) => {
+    const {id, title, author, status} = req.body;
+    if(!id || !title || !author || !status){
+        return res.status(400).send({message: 'Missing required field'})
+    }
+    books[id] = {id, title, author, status};
+    res.status(201).send(books[id]);   
 })
-.get( (req, res) => {
-    res.send('View books');
+.get('/books', (req, res) => {
+    res.send(Object.values(books[id]));
 })
-.post((req, res) => {
-    res.send('Add you favorites');
+.get('/books/:id', (req, res) => {
+    const book = books[req.params.id];
+    book? res.send(book) : res.status(404).send({message: 'Book not found'})
 })
-.put((req, res) => {
-    res.send('Books[index]');
+.put('./books/:id', (req, res) => {
+    if(books[req.params.id]) {
+        return res.status(404).send({message: 'Book not found'})
+    }
+    books[req.params.id] = {...books[req.params.id], ...req.body};
+    res.send(books[req.params.id]);
 })
-.delete((req, res) => {
-    res.send('Delete book');
+.delete('/books/:id', (req, res) => {
+    if(!books[req.params.id]) {
+        return res.status(404).send({message: 'Book not found'})
+    }
+    const deleteBook = books[req.params.id];
+    delete books[req.params.id];
+    res.send(deleteBook);
 });
 
 export default router;
